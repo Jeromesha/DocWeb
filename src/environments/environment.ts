8bf7c89c3203f7c25865979b3322910eaab40306
vathula anand <vathula.a@xenovec.com>
2023-09-27 13:49:39 +0530

    timesheet page update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   apiBaseUrl: 'http://eazypmv2api.xenovex.com',
+  //apiBaseUrl: 'https://localhost:7262',
+
   tokenEndPoint: "/api/token",
   production: false,
   environment: "Local",
