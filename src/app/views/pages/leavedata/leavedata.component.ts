1c285f1ec672f19be2113575e1db050d9be983af
Tamil Arasu Selvaraj <tamilarasu.s@xenovex.com>
2024-02-01 17:28:18 +0530

    leave data commit

--- src/app/views/pages/leavedata/leavedata.component.ts
+++ src/app/views/pages/leavedata/leavedata.component.ts
@@ -21,6 +21,8 @@ export class LeavedataComponent implements OnInit {
   data: any;
   fileevent: any;
   attachment: any[];
+  errorMessage: string;
+  errorMessagetype: boolean = false;
 
 
   constructor(
@@ -37,15 +39,17 @@ export class LeavedataComponent implements OnInit {
 
   initializeValidators() {
     this.form = this.formBuilder.group({
-      files: this.formBuilder.array([], Validators.required),
+      files: [[], Validators.required],
     });
   }
   updateFileName(event: any): void {
     this.fileevent = event;
-    const fileName = event.target.files[0]?.name;
+    const files: FileList = event.target.files;
     const label = document.querySelector('.custom-file-label');
-    if (fileName && label) {
-      label.textContent = fileName;
+
+    if (files && files.length > 0 && label) {
+      const fileNames = Array.from(files).map(file => file.name).join(', ');
+      label.textContent = fileNames;
     }
   }
   onClear() {
@@ -59,29 +63,58 @@ export class LeavedataComponent implements OnInit {
     }
     this.form.reset();
   }
+
   onSubmit() {
     debugger;
     if (this.form.valid) {
-      var file = this.fileevent.target.files[0];
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        var base64Data: string = reader.result as string;
-        console.log(base64Data);
-        this.attachment = [];
-        // var fileName: file.name;
-        // var fileType: file.type;
-  
-        this.attachment.push({
-          // fileName: fileName,
-          base64Content: base64Data,
-          // fileType:fileType,
-          statusType: 1
-        });
-  
-        // Now 'this.attachment' contains an object with file information including the base64 content
-        console.log(this.attachment);
-      };
+      //   const files :File[]=this.fileevent.target.files[0]
+      //   //const files: File[] = this.form.get('files').value;
+      //   for (let i = 0; i < files.length; i++) {
+      //   // var file = this.fileevent.target.files[0];
+      //   const file = files[i];
+      //   var reader = new FileReader();
+      //   reader.readAsDataURL(file);
+      //   reader.onload = () => {
+      //     var base64Data: string = reader.result as string;
+      //     console.log(base64Data);
+      //     this.attachment = [];
+      //     // var fileName: file.name;
+      //     // var fileType: file.type;
+
+      //     this.attachment.push({
+      //       // fileName: fileName,
+      //       base64Content: base64Data,
+      //       // fileType:fileType,
+      //       statusType: 1
+      //     });
+      //     console.log(this.attachment);
+
+      //   };
+      // }
+      const files: FileList = this.fileevent.target.files;
+      this.attachment = []; 
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+        var fileName =file.name;
+        var fileType=file.type;
+
+        reader.onload = () => {
+          const base64Data: string = reader.result as string;
+          console.log(base64Data);
+
+          this.attachment.push({
+            base64Content: base64Data,
+            statusType: 1,
+            fileName:fileName,
+            fileType:fileType
+          });
+
+          console.log(this.attachment);
+        };
+
+        reader.readAsDataURL(file);
+      }
     }
     else {
       debugger
